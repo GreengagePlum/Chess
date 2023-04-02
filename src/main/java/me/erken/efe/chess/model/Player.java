d0131687b1b676249e83a11e8279c5e559b4b76a
Efe ERKEN <efeerkenn@hotmail.com>
2023-04-02 18:07:30 +0200

    :wrench: UPDATE: Add selection attribute
    Added a new attribute to remember player square selection.

--- src/main/java/me/erken/efe/chess/model/Player.java
+++ src/main/java/me/erken/efe/chess/model/Player.java
@@ -4,11 +4,13 @@ public class Player {
     private static int iDCounter = 0;
     private final Color color;
     private final int id;
+    private Square selection;
 
     public Player(Color color) {
         this.color = color;
         id = iDCounter;
         iDCounter++;
+        selection = null;
     }
 
     public Color getColor() {
@@ -19,12 +21,22 @@ public class Player {
         return id;
     }
 
+    private void setSelection(Square square) {
+        selection = square;
+    }
+
+    private void unsetSelection() {
+        selection = null;
+    }
+
     public void makeSelection(Square square, Board board) {
         Piece p = square.getPiece();
         if (p == null || p.getColor() != color) {
             board.clearSquares();
+            this.unsetSelection();
         } else {
             board.selectSquare(square);
+            this.setSelection(square);
         }
     }
 
