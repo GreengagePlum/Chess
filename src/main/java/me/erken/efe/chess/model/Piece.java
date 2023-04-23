d9dab832099e1556989fc3d80f51863e20edd55d
Efe ERKEN <efeerkenn@hotmail.com>
2023-04-23 19:38:06 +0200

    :wrench: UPDATE: Add new fields
    Added new fields and their relative methods, removed useless ones.

--- src/main/java/me/erken/efe/chess/model/Piece.java
+++ src/main/java/me/erken/efe/chess/model/Piece.java
@@ -1,25 +1,61 @@
 package me.erken.efe.chess.model;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
 public abstract class Piece {
-    private static int idCounter = 0;
-    private final int id;
     private final Color color;
+    protected List<Coordinates> legalPositions;
+    protected List<Coordinates> attackingPositions;
+    private boolean kingProtector;
+    private Piece kingProtectorCausingPiece;
 
     public Piece(Color color) {
         this.color = color;
-        id = idCounter;
-        idCounter++;
+        legalPositions = new LinkedList<>();
+        attackingPositions = new LinkedList<>();
+        kingProtector = false;
+        kingProtectorCausingPiece = null;
     }
 
     public Color getColor() {
         return color;
     }
 
-    public int getId() {
-        return id;
+    public ListIterator<Coordinates> getLegalPositions() {
+        return legalPositions.listIterator();
     }
 
-    public boolean coordCheck(Coordinates coords) {
+    public ListIterator<Coordinates> getAttackingPositions() {
+        return attackingPositions.listIterator();
+    }
+
+    public Piece getKingProtectorCausingPiece() {
+        return kingProtectorCausingPiece;
+    }
+
+    public void setKingProtectorCausingPiece(Piece p) {
+        kingProtectorCausingPiece = p;
+    }
+
+    public void clearKingProtectorCausingPiece() {
+        kingProtectorCausingPiece = null;
+    }
+
+    public boolean legalPositionsContains(Coordinates coordinates) {
+        return legalPositions.contains(coordinates);
+    }
+
+    public boolean isKingProtector() {
+        return kingProtector;
+    }
+
+    public void setKingProtector(boolean kingProtector) {
+        this.kingProtector = kingProtector;
+    }
+
+    public boolean coordinateCheck(Coordinates coords) {
         return coords.x < Board.WIDTH && coords.x >= 0 && coords.y < Board.HEIGHT && coords.y >= 0;
     }
 
