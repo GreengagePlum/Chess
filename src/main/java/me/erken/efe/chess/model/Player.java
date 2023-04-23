d15a1874d5a6d081d59029609f9a8c69bd6c555d
Efe ERKEN <efeerkenn@hotmail.com>
2023-04-23 19:50:57 +0200

    :wrench: UPDATE: Fix method rename
    Renamed used methods and removed IDs.

--- src/main/java/me/erken/efe/chess/model/Player.java
+++ src/main/java/me/erken/efe/chess/model/Player.java
@@ -1,24 +1,16 @@
 package me.erken.efe.chess.model;
 
 public class Player {
-    private static int iDCounter = 0;
     private final Color color;
-    private final int id;
     private Square selection;
 
     public Player(Color color) {
         this.color = color;
-        id = iDCounter;
-        iDCounter++;
         selection = null;
     }
 
     public Color getColor() {
         return color;
-    }
-
-    public int getId() {
-        return id;
     }
 
     private void setSelection(Square square) {
@@ -32,7 +24,7 @@ public class Player {
     public void makeSelection(Square square, Board board) {
         Piece p = square.getPiece();
         if (p == null || p.getColor() != color) {
-            board.clearSquares();
+            board.clearStateSquares();
             this.unsetSelection();
         } else {
             board.selectSquare(square);
@@ -49,7 +41,7 @@ public class Player {
             System.out.println("Coup impossible");
         } finally {
             this.unsetSelection();
-            board.clearSquares();
+            board.clearStateSquares();
         }
     }
 }
