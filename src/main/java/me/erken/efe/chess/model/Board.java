b8dee71db3bfc9c3385f323ac435d63d9fcbb57c
Efe ERKEN <efeerkenn@hotmail.com>
2023-04-24 23:22:38 +0200

    :wrench: UPDATE: Add new methods
    New functionalities to calculate all pieces, minor refactoring.

--- src/main/java/me/erken/efe/chess/model/Board.java
+++ src/main/java/me/erken/efe/chess/model/Board.java
@@ -1,5 +1,7 @@
 package me.erken.efe.chess.model;
 
+import java.util.ListIterator;
+
 public class Board {
     public static final int WIDTH = 8;
     public static final int HEIGHT = 8;
@@ -96,12 +98,9 @@ public class Board {
         return (color == Color.BLACK) ? kingBlack : kingWhite;
     }
 
-    public void setKingStatus(Color color, boolean inCheckStatus) {
-        if ((color == Color.BLACK)) {
-            kingBlack.setInCheck(inCheckStatus);
-        } else {
-            kingWhite.setInCheck(inCheckStatus);
-        }
+    public void clearKingsCheck() {
+        kingBlack.setInCheck(false);
+        kingWhite.setInCheck(false);
     }
 
     private void highlightMoves(Piece piece) {
@@ -126,7 +125,7 @@ public class Board {
         }
     }
 
-    private void clearDangerSquares() {
+    public void clearDangerSquares() {
         for (int i = 0; i < HEIGHT; i++) {
             for (int j = 0; j < WIDTH; j++) {
                 grid[i][j].setDanger(SquareDanger.PEACEFUL);
@@ -134,26 +133,55 @@ public class Board {
         }
     }
 
-    private void setAttackedSquares(Coordinates[] attackPos, Color origin) {
-        for (int i = 0; attackPos[i] != null; i++) {
-            Square current = getSquare(attackPos[i]);
-            if (current.getDanger() == SquareDanger.PEACEFUL) {
-                current.setDanger((origin == Color.BLACK) ? SquareDanger.BLACK_ATTACKING : SquareDanger.WHITE_ATTACKING);
+    private void setAttackedSquares(ListIterator<Coordinates> attackPos, Color origin) {
+        while (attackPos.hasNext()) {
+            Coordinates currPos = attackPos.next();
+            Square currSq = getSquare(currPos);
+            if (currSq.getDanger() == SquareDanger.PEACEFUL) {
+                currSq.setDanger((origin == Color.BLACK) ? SquareDanger.BLACK_ATTACKING : SquareDanger.WHITE_ATTACKING);
             } else {
-                current.setDanger(SquareDanger.BOTH_ATTACKING);
+                currSq.setDanger(SquareDanger.BOTH_ATTACKING);
             }
         }
     }
 
     public void updateDangerSquares() {
-        clearDangerSquares();
         for (int i = 0; i < HEIGHT; i++) {
             for (int j = 0; j < WIDTH; j++) {
                 Piece p = grid[i][j].getPiece();
                 if (p != null) {
-                    setAttackedSquares(p.attackingPositions.toArray(new Coordinates[0]), p.getColor());
+                    setAttackedSquares(p.getAttackingPositions(), p.getColor());
                 }
             }
         }
+    }
+
+    public void calculateAllPieces(Color firstColorToProcess) {
+        calculateColorPieces(firstColorToProcess);
+        calculateColorPieces((firstColorToProcess == Color.BLACK) ? Color.WHITE : Color.BLACK);
+    }
+
+    private void calculateColorPieces(Color currentColor) {
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                Piece p = grid[y][x].getPiece();
+                if (p != null && p.getColor() == currentColor) {
+                    p.updateAllPositions(new Coordinates(x, y), this);
+                }
+            }
+        }
+    }
+
+    public int countAvailableMoves(Color color) {
+        int moveCount = 0;
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                Piece p = grid[y][x].getPiece();
+                if (p != null && p.getColor() == color) {
+                    moveCount += p.legalPositionsCount();
+                }
+            }
+        }
+        return moveCount;
     }
 }
