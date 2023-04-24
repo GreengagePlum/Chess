42f81553d65629807841fbd616cf5d52528e4a96
Efe ERKEN <efeerkenn@hotmail.com>
2023-04-24 19:16:35 +0200

    :wrench: UPDATE: Add new tests
    Added new test for the Rook piece and removed Piece class test.

--- src/test/java/me/erken/efe/chess/model/TestRook.java
+++ src/test/java/me/erken/efe/chess/model/TestRook.java
@@ -11,10 +11,8 @@ public class TestRook {
         Board b = new Board();
         Coordinates source = new Coordinates(0, 7);
         Rook r = (Rook) b.getSquare(source).getPiece();
-        Coordinates[] moves = r.movablePositions(source, b);
-        int length = 0;
-        for (; length < moves.length && moves[length] != null; length++) ;
-        Assertions.assertEquals(0, length);
+        r.updateLegalPositions(source, b);
+        Assertions.assertEquals(0, r.legalPositions.size());
     }
 
     @Test
@@ -24,10 +22,8 @@ public class TestRook {
         Coordinates source = new Coordinates(2, 4);
         Rook r = new Rook(Color.WHITE);
         b.getSquare(source).setPiece(r);
-        Coordinates[] moves = r.movablePositions(source, b);
-        int length = 0;
-        for (; length < moves.length && moves[length] != null; length++) ;
-        Assertions.assertEquals(11, length);
+        r.updateLegalPositions(source, b);
+        Assertions.assertEquals(11, r.legalPositions.size());
     }
 
     @Test
@@ -37,9 +33,44 @@ public class TestRook {
         Coordinates source = new Coordinates(7, 2);
         Rook r = new Rook(Color.WHITE);
         b.getSquare(source).setPiece(r);
-        Coordinates[] moves = r.movablePositions(source, b);
-        int length = 0;
-        for (; length < moves.length && moves[length] != null; length++) ;
-        Assertions.assertEquals(11, length);
+        r.updateLegalPositions(source, b);
+        Assertions.assertEquals(11, r.legalPositions.size());
+    }
+
+    @Test
+    @DisplayName("Move set when king is in check")
+    void moveSetKingInCheck() {
+        Board b = new Board();
+        Bishop p = new Bishop(Color.WHITE);
+        b.getSquare(new Coordinates(1, 5)).setPiece(p);
+        b.getSquare(new Coordinates(4, 2)).setPiece(b.getKing(Color.BLACK));
+        b.getSquare(new Coordinates(4, 0)).setPiece(null);
+        Rook r = new Rook(Color.BLACK);
+        b.getSquare(new Coordinates(4, 4)).setPiece(r);
+        Coordinates source = new Coordinates(4, 4);
+        r.updateAllPositions(source, b);
+        Assertions.assertEquals(10, r.legalPositions.size());
+        p.updateAllPositions(new Coordinates(1, 5), b);
+        r.updateAllPositions(source, b);
+        Assertions.assertEquals(2, r.legalPositions.size());
+    }
+
+    @Test
+    @DisplayName("Move set when king protector")
+    void moveSetKingProtector() {
+        Board b = new Board();
+        Bishop p = new Bishop(Color.WHITE);
+        b.getSquare(new Coordinates(1, 5)).setPiece(p);
+        b.getSquare(new Coordinates(4, 2)).setPiece(b.getKing(Color.BLACK));
+        b.getSquare(new Coordinates(4, 0)).setPiece(null);
+        Rook r = new Rook(Color.BLACK);
+        b.getSquare(new Coordinates(3, 3)).setPiece(r);
+        Coordinates source = new Coordinates(3, 3);
+        r.updateAllPositions(source, b);
+        Assertions.assertEquals(11, r.legalPositions.size());
+        p.updateAllPositions(new Coordinates(1, 5), b);
+        p.setKingProtectorsInPath(new Coordinates(1, 5), b);
+        r.updateAllPositions(source, b);
+        Assertions.assertEquals(0, r.legalPositions.size());
     }
 }
