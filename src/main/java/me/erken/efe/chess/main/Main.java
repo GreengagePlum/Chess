acd1fedfcec1a20606aa6e4d64173cb001deae93
Efe ERKEN <efeerkenn@hotmail.com>
2023-04-28 00:54:45 +0200

    :wrench: UPDATE: Finalize Main class
    Final touches on the Main class, the entry point to the program.

---
+++ src/main/java/me/erken/efe/chess/main/Main.java
@@ -0,0 +1,9 @@
+package me.erken.efe.chess.main;
+
+import me.erken.efe.chess.view.GUIJavaFX;
+
+public class Main {
+    public static void main(String[] args) {
+        GUIJavaFX.main(args);
+    }
+}
